f8ccc3b087ec58057f3d7b10b5284119a0597896
svtslvchk <solovevsr@gmail.com>
2026-03-14 18:00:57 +0300

    Splay v1

--- TreeDataStructures/Implementations/Splay/SplayTree.cs
+++ TreeDataStructures/Implementations/Splay/SplayTree.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Transactions;
 using TreeDataStructures.Implementations.BST;
 
 namespace TreeDataStructures.Implementations.Splay;
@@ -8,21 +9,44 @@ public class SplayTree<TKey, TValue> : BinarySearchTree<TKey, TValue>
 {
     protected override BstNode<TKey, TValue> CreateNode(TKey key, TValue value)
         => new(key, value);
-    
+
     protected override void OnNodeAdded(BstNode<TKey, TValue> newNode)
     {
         Splay(newNode);
     }
-    
+
     protected override void OnNodeRemoved(BstNode<TKey, TValue>? parent, BstNode<TKey, TValue>? child)
     {
         Splay(child ?? parent);
     }
-    
+
     // доделать 
     public override bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
     {
-        throw new NotImplementedException(); 
+        // идем до ключа и кидаем вверх последний посещенный элемент (даже если не нашли)
+        BstNode<TKey, TValue>? current = this.Root;
+        BstNode<TKey, TValue>? last = null;
+        while (current != null)
+        {
+            last = current;
+            int cmp = Comparer.Compare(key, current.Key);
+            if (cmp == 0)
+            {
+                value = current.Value;
+                Splay(current);
+                return true;
+            }
+
+            current = (cmp < 0) ? current.Left : current.Right;
+        }
+
+        if (last != null)
+        {
+            Splay(last);
+        }
+
+        value = default;
+        return false;
     }
 
     private void Zig(BstNode<TKey, TValue> x, BstNode<TKey, TValue> p)
@@ -77,5 +101,5 @@ public class SplayTree<TKey, TValue> : BinarySearchTree<TKey, TValue>
             }
         }
     }
-    
+
 }
